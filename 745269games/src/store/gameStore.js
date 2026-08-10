f243b09272e43bd4bbe54d4d934cbf9097015e51
1763686131 <1763686131@qq.com>
2026-08-10 18:10:49 +0800

    1.6.1 部署

--- 745269games/src/store/gameStore.js
+++ 745269games/src/store/gameStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-const API_BASE_URL = 'http://localhost:5269'
+const API_BASE_URL = 'http://192.168.31.236:5269'
 
 export const useGameStore = defineStore('game', () => {
   const isLoading = ref(false)
